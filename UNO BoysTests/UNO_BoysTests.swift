9a02de6e105016f4fc6cb9b3de1d12294d437875
Nicolas Yepes <nicolasyepespulido@gmail.com>
2019-07-12 13:05:30 -0500

    Initial Commit

---
+++ UNO BoysTests/UNO_BoysTests.swift
@@ -0,0 +1,34 @@
+//
+//  UNO_BoysTests.swift
+//  UNO BoysTests
+//
+//  Created by Nicolas Yepes on 7/12/19.
+//  Copyright © 2019 UNO Boys. All rights reserved.
+//
+
+import XCTest
+@testable import UNO_Boys
+
+class UNO_BoysTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
