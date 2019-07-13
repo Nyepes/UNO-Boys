c5f91a99ae982f49cb03cffe4506fcd42ea3a44d
Allan Zhang <allanwzhang04@gmail.com>
2019-07-13 13:00:46 -0500

    Made new files and connected to storyboard

---
+++ UNO Boys/ActualQuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ActualQuestionViewController.swift
+//  UNO Boys
+//
+//  Created by Allan Zhang on 7/13/19.
+//  Copyright © 2019 UNO Boys. All rights reserved.
+//
+
+import UIKit
+
+class ActualQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
