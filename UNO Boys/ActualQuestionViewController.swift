ced718fc4d48965bcaaef8b304ecad60f315f9b3
Allan Zhang <allanwzhang04@gmail.com>
2019-07-13 13:16:04 -0500

    added code from my trivia app

--- UNO Boys/ActualQuestionViewController.swift
+++ UNO Boys/ActualQuestionViewController.swift
@@ -9,22 +9,80 @@
 import UIKit
 
 class ActualQuestionViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answer1Label: UILabel!
+    @IBOutlet weak var answer2Label: UILabel!
+    @IBOutlet weak var answer3Label: UILabel!
+    @IBOutlet weak var answer4Label: UILabel!
+    
+    var question = [String: String]()
+    var correctIndex = 0
+    var correctLabel = UILabel()
+    var labelArray = [UILabel]()
+    
     override func viewDidLoad() {
+        labelArray = [answer1Label, answer2Label, answer3Label, answer4Label]
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        questionLabel.text = question["question"]
+        correctIndex = Int.random(in: 0...3)
+        switch correctIndex {
+        case 0:
+            answer1Label.text = question["correct"]
+            correctLabel = answer1Label
+        case 1:
+            answer2Label.text = question["correct"]
+            correctLabel = answer2Label
+        case 2:
+            answer3Label.text = question["correct"]
+            correctLabel = answer3Label
+        default:
+            answer4Label.text = question["correct"]
+            correctLabel = answer4Label
+        }
+        var curr = 1
+        if(answer1Label.text!.count == 0) {
+            answer1Label.text = question["wrong\(curr)"]
+            curr += 1
+        }
+        if(answer2Label.text!.count == 0) {
+            answer2Label.text = question["wrong\(curr)"]
+            curr += 1
+        }
+        if(answer3Label.text!.count == 0) {
+            answer3Label.text = question["wrong\(curr)"]
+            curr += 1
+        }
+        if(answer4Label.text!.count == 0) {
+            answer4Label.text = question["wrong\(curr)"]
+            curr += 1
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func touhced(_ sender: UITapGestureRecognizer) {
+        let selectedPoint = sender.location(in: view)
+        var count = 0
+        for label in labelArray {
+            if(label.frame.contains(selectedPoint)) {
+                checkAnswer(labelChosen: label, num: count)
+                return
+            }
+            count += 1
+        }
     }
-    */
-
+    
+    var answered = false
+    
+    func checkAnswer(labelChosen: UILabel, num: Int) {
+        if(answered) {
+            return
+        }
+        if(num == correctIndex) {
+            labelChosen.backgroundColor = .green
+        } else {
+            labelChosen.backgroundColor = .red
+            correctLabel.backgroundColor = .green
+        }
+        answered = true
+    }
 }
--- UNO Boys/GeneralCategoriesViewController.swift
+++ UNO Boys/GeneralCategoriesViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class GeneralCategoriesViewController: UITableViewController {
 
+    var genre = ["Sports", "History", "Science", "Music", "Politics"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "Trivia!"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return genre.count
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = genre[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! GeneralQuestionsViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.genre = genre[index!]
+    }
 
 }
